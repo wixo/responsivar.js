11ae2e834d470417e69be023e1e18aab0de6d271
Juan La Jara <juanlajara@gmail.com>
2013-07-14 20:27:11 -0500

    first commit

---
+++ responsivar-min.js
@@ -0,0 +1,8 @@
+//
+// Responsivar.js 0.0.1
+// Store variables in your media queries
+// http://github.com/wixo/responsivar
+//
+
+(function(e,g,f){f=function(){var c={cssTest:{},mQListeners:[],identifier:"",stylesheet:null,findMatch:function(a){var b=c.stylesheet,d;if(a.matches)for(b=b.sheet.cssRules,d=b.length;d--;)if(-1!==b[d].cssText.indexOf(a.media)){b=b[d].cssRules[0].selectorText.substring(1);c.identifier=b;a.cb(b);break}},init:function(a){var b=a.mediaQueries,d=b.length,f=a.change;c.stylesheet=g.getElementById(a.stylesheetName||"responsivar");if(e.matchMedia||e.msMatchMedia)for(;d--;)if(a=b[d].substr(7),a=e.matchMedia(a)||
+e.msMatchMedia(a))a.cb=f,a.addListener(c.findMatch),c.findMatch(a),c.mQListeners.push(a)}};return{init:c.init}}();e.responsivar=f.init})(window,window.document);
---
+++ responsivar.js
@@ -0,0 +1,82 @@
+//
+// Responsivar.js 0.0.1
+// Store variables in your media queries
+// http://github.com/wixo/responsivar
+//
+
+/*jshint laxcomma:true, smarttabs:true, forin:true, noarg:true, noempty:true, eqeqeq:true, laxbreak:true, bitwise:true, strict:true, undef:true, unused:true, curly:true, browser:true, indent:4, maxerr:50 */
+( function ( global, document, undefined ) {
+
+	"use strict";
+
+	var MQ = ( function () {
+
+		var MQ = {}
+		  , self = MQ
+		  ;
+
+		MQ.cssTest     = {};
+		MQ.mQListeners = [];
+		MQ.identifier  = '';
+		MQ.stylesheet  = null;
+
+		MQ.findMatch = function ( mql ) {
+			var stylesheet = self.stylesheet
+			  , rules
+			  , rulesLength
+			  , result
+			  ;
+
+			if ( mql.matches ) {
+				rules = stylesheet.sheet.cssRules;
+				rulesLength = rules.length;
+
+				while ( rulesLength-- ) {
+					if ( rules[rulesLength].cssText.indexOf( mql.media ) !== -1 ) {
+						result = rules[rulesLength].cssRules[0].selectorText.substring(1);
+						self.identifier = result;
+						mql.cb( result );
+						break;
+					}
+				}
+
+			}
+
+		};
+
+		MQ.init = function ( givens ) {
+			var mediaQueries = givens.mediaQueries
+			  , mqLength     = mediaQueries.length
+			  , callback     = givens.change
+			  , fragment
+			  , mql
+			  ;
+
+			self.stylesheet = document.getElementById( givens.stylesheetName || 'responsivar' );
+
+			if ( global.matchMedia || global.msMatchMedia ) {
+
+				while ( mqLength-- ) {
+					fragment = mediaQueries[mqLength].substr(7);
+					mql = global.matchMedia( fragment ) || global.msMatchMedia( fragment );
+
+					if ( mql ) {
+						mql.cb = callback;
+						mql.addListener( self.findMatch );
+						self.findMatch( mql );
+
+						self.mQListeners.push( mql );
+					}
+
+				}
+
+			}
+
+		};
+
+		return { init: self.init };
+
+	} )();
+
+	global.responsivar = MQ.init;
+} )( window, window.document );
